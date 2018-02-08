9afd56abaa939405e5d9d59119dd34b447ba7783
Samuel Lima <samuel@syncsoftwares.com.br>
2018-02-08 16:12:06 -0200

    #67567 - Ajustei a tela da impressão de etiqueta para não exibir o ícone de cancelamento de etiqueta caso o funcionário não tenha permissão para realizar essa função.

--- Glass.Data/PCP/DAL/ImpressaoEtiquetaDAO.cs
+++ Glass.Data/PCP/DAL/ImpressaoEtiquetaDAO.cs
@@ -3342,6 +3342,14 @@ namespace Glass.Data.DAL
             return string.Join(",", Array.ConvertAll(ids.ToArray(), x => x.ToString()));
         }
 
+        /// <summary>
+        /// Recupera o funcionário que gerou a impressão.
+        /// </summary>
+        public int ObterIdFunc(GDASession session, int idImpressao)
+        {
+            return ObtemValorCampo<int>(session, "IdFunc", string.Format("IdImpressao={0}", idImpressao));
+        }
+
         #endregion
 
         #region Atualiza situação da impressão
