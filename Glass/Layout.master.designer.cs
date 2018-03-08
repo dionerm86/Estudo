dc1e8ed8fd07bf21bb7cd21a17f8485d75170890
Felipe Ribeiro <felipe@syncsoftwares.com.br>
2018-03-08 09:28:41 -0300

    #66842 Criação de tela para exibir as dlls utilizadas

--- Glass/Layout.master.designer.cs
+++ Glass/Layout.master.designer.cs
@@ -49,13 +49,13 @@ namespace Glass.UI.Web {
         protected global::System.Web.UI.WebControls.Image imgLogoWebGlass;
         
         /// <summary>
-        /// lblVersao control.
+        /// lnkVersao control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblVersao;
+        protected global::System.Web.UI.WebControls.LinkButton lnkVersao;
         
         /// <summary>
         /// lblTelSuporte control.
