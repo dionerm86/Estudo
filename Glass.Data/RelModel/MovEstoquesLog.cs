d8f5baeedf46eaf1879ffb3aa3a2946612c62f23
Felipe Ribeiro <felipe@syncsoftwares.com.br>
2018-04-19 16:24:04 -0300

    #63803 Log de movimentação de notas fiscais

---
+++ Glass.Data/RelModel/MovEstoquesLog.cs
@@ -0,0 +1,46 @@
+﻿using GDA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glass.Data.RelDAL;
+
+namespace Glass.Data.RelModel
+{
+    [PersistenceBaseDAO(typeof(MovEstoquesLogDAO))]
+    public class MovEstoquesLog
+    {
+        [PersistenceProperty("IDPRODNF")]
+        public uint IdProdNf { get; set; }
+
+        [PersistenceProperty("CODPROD")]
+        public string CodProd { get; set; }
+
+        [PersistenceProperty("DESCRICAOPROD")]
+        public string DescricaoProd { get; set; }
+
+        [PersistenceProperty("MOVESTOQUEFISCAL")]
+        public uint? MovEstoqueFiscal { get; set; }
+
+        [PersistenceProperty("TIPOMOVFISCAL")]
+        public string TipoMovFiscal { get; set; }
+
+        [PersistenceProperty("QTDEMOVFISCAL")]
+        public uint? QtdeMovFiscal { get; set; }
+
+        [PersistenceProperty("DATAMOVFISCAL")]
+        public DateTime? DataMovFiscal { get; set; }
+
+        [PersistenceProperty("MOVESTOQUEREAL")]
+        public uint? MovEstoqueReal { get; set; }
+
+        [PersistenceProperty("TIPOMOVREAL")]
+        public string TipoMovReal { get; set; }
+
+        [PersistenceProperty("QTDEMOVREAL")]
+        public uint? QtdeMovReal { get; set; }
+
+        [PersistenceProperty("DATAMOVREAL")]
+        public DateTime? DataMovReal { get; set; }
+    }
+}
