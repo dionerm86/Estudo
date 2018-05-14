c4dea4da618618704d1e713f078eec842141ba83
Gabriel Silva <gabriel@syncsoftwares.com.br>
2018-05-14 17:40:51 -0300

    #72387 Alterei para que o sistema permita vincular a Nota Fiscal se esta estiver vinculada a um MDFe cancelado.

--- Glass.Data/Fiscal/DAL/MDFe/NFeCidadeDescargaMDFeDAO.cs
+++ Glass.Data/Fiscal/DAL/MDFe/NFeCidadeDescargaMDFeDAO.cs
@@ -63,7 +63,16 @@ namespace Glass.Data.DAL
         /// <returns></returns>
         public bool VerificarNfeJaInclusa(int idNfe)
         {
-            return objPersistence.ExecuteSqlQueryCount("SELECT IdCidadeDescarga FROM nfe_cidade_descarga_mdfe WHERE IdNfe=" + idNfe) > 0;
+            return objPersistence.ExecuteSqlQueryCount(string.Format(@"SELECT 
+                ncdm.IdCidadeDescarga
+            FROM
+                nfe_cidade_descarga_mdfe ncdm
+                    INNER JOIN
+                cidade_descarga_mdfe cdm ON (ncdm.IdCidadeDescarga = cdm.IdCidadeDescarga)
+                    INNER JOIN
+                manifesto_eletronico me ON (me.Situacao <> 3 AND
+                    me.IdManifestoEletronico = cdm.IdManifestoEletronico)
+            AND ncdm.IdNfe = {0}", idNfe)) > 0;
         }
 
         #region Metodos Sobrescritos
