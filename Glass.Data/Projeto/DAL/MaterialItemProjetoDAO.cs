f457c19ec5b25a6343379e4b724dfb6534be314e
Bruce Alexsander <bruce@syncsoftwares.com.br>
2018-07-25 09:34:09 -0300

    Aplicação de patch dos chamados 76380 e 74479

--- Glass.Data/Projeto/DAL/MaterialItemProjetoDAO.cs
+++ Glass.Data/Projeto/DAL/MaterialItemProjetoDAO.cs
@@ -1619,11 +1619,11 @@ namespace Glass.Data.DAL
                     foreach (var prodBenef in objInsert.Beneficiamentos)
                     {
                         if (BenefConfigDAO.Instance.GetElement(prodBenef.IdBenefConfig).TipoControle == Data.Model.TipoControleBenef.Bisote &&
-                            objInsert.Altura < tamanhoMinimoBisote && objInsert.Largura < tamanhoMinimoBisote)
+                            objInsert.Altura < tamanhoMinimoBisote || objInsert.Largura < tamanhoMinimoBisote)
                             retorno += $"A altura ou largura minima para peças com bisotê é de {tamanhoMinimoBisote}mm.";
 
                         if (BenefConfigDAO.Instance.GetElement(prodBenef.IdBenefConfig).TipoControle == Data.Model.TipoControleBenef.Lapidacao &&
-                            objInsert.Altura < tamanhoMinimoLapidacao && objInsert.Largura < tamanhoMinimoLapidacao)
+                            objInsert.Altura < tamanhoMinimoLapidacao || objInsert.Largura < tamanhoMinimoLapidacao)
                             retorno += $"A altura ou largura minima para peças com lapidação é de {tamanhoMinimoLapidacao}mm.";
                     }
                 }
@@ -1795,11 +1795,11 @@ namespace Glass.Data.DAL
                     foreach (var prodBenef in objUpdate.Beneficiamentos)
                     {
                         if (BenefConfigDAO.Instance.GetElement(prodBenef.IdBenefConfig).TipoControle == Data.Model.TipoControleBenef.Bisote &&
-                            objUpdate.Altura < tamanhoMinimoBisote && objUpdate.Largura < tamanhoMinimoBisote)
+                            objUpdate.Altura < tamanhoMinimoBisote || objUpdate.Largura < tamanhoMinimoBisote)
                             retorno += $"A altura ou largura minima para peças com bisotê é de {tamanhoMinimoBisote}mm.";
 
                         if (BenefConfigDAO.Instance.GetElement(prodBenef.IdBenefConfig).TipoControle == Data.Model.TipoControleBenef.Lapidacao &&
-                            objUpdate.Altura < tamanhoMinimoLapidacao && objUpdate.Largura < tamanhoMinimoLapidacao)
+                            objUpdate.Altura < tamanhoMinimoLapidacao || objUpdate.Largura < tamanhoMinimoLapidacao)
                             retorno += $"A altura ou largura minima para peças com lapidação é de {tamanhoMinimoLapidacao}mm.";
                     }
                 }
