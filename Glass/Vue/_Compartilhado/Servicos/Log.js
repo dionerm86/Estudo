1f2c6b57d905c557b6f20b6db1699449c5482637
André <andre@syncsoftwares.com.br>
2018-09-08 11:23:41 -0300

    Implementação do log de cancelamento

--- Glass/Vue/_Compartilhado/Servicos/Log.js
+++ Glass/Vue/_Compartilhado/Servicos/Log.js
@@ -34,6 +34,35 @@ Servicos.Log = (function(http) {
       obterTabelas: function () {
         return http().get(API + 'alteracao/tabelas')
       }
+    },
+
+    /**
+     * Objeto com os métodos para o log de cancelamento.
+     */
+    Cancelamento: {
+      /**
+       * Verifica se um item (de uma tabela específica) possui log de cancelamento.
+       * Caso seja informado o campo, verifica se o item possui algum log no campo desejado.
+       * @param {!number} idTabela O identificador da tabela do item.
+       * @param {!number} idItem O identificador do item.
+       * @param {?string} [campo=null] O nome de um campo específico que pode ser verificado.
+       * @returns {Promise} Uma promise com o resultado da busca.
+       */
+      verificarLogItem: function (idTabela, idItem, campo) {
+        return http().get(API + 'cancelamento/' + idTabela + '/possuiLog/' + idItem, {
+          params: {
+            campo
+            }
+        });
+      },
+
+      /**
+       * Recupera a lista de tabelas passíveis de log de cancelamento do sistema.
+       * @returns {Promise} Uma promise com o resultado da busca.
+       */
+      obterTabelas: function () {
+        return http().get(API + 'cancelamento/tabelas')
+      }
     }
   };
 })(function() {
