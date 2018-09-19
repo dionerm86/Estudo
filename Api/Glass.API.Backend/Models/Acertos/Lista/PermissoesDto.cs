1afb212cdc580be69ee11e9682cf48c215d1b78e
André <andre@syncsoftwares.com.br>
2018-09-19 16:33:10 -0300

    Correção nas pastas das classes de permissões de acerto e cheque

--- Api/Glass.API.Backend/Models/Acertos/Lista/PermissoesDto.cs
+++ Api/Glass.API.Backend/Models/Acertos/Lista/PermissoesDto.cs
@@ -5,54 +5,33 @@
 using Newtonsoft.Json;
 using System.Runtime.Serialization;
 
-namespace Glass.API.Backend.Models.Cheques.Lista
+namespace Glass.API.Backend.Models.Acertos.Lista
 {
     /// <summary>
-    /// Classe que encapsula os dados de permissão da tela de cheques.
+    /// Classe que encapsula os dados de permissão da tela de listagem de acertos.
     /// </summary>
     [DataContract(Name = "Permissoes")]
     public class PermissoesDto
     {
         /// <summary>
-        /// Obtém ou define um valor que indica se será permitido alterar dados do cheque.
+        /// Obtém ou define um valor que indica se será permitido cancelar o acerto.
         /// </summary>
         [DataMember]
-        [JsonProperty("alterarDadosCheque")]
-        public bool AlterarDadosCheque { get; set; }
+        [JsonProperty("cancelar")]
+        public bool Cancelar { get; set; }
 
         /// <summary>
-        /// Obtém ou define um valor que indica se será permitido alterar a data de vencimento do cheque.
+        /// Obtém ou define um valor que indica se será permitido imprimir a nota promissória do acerto.
         /// </summary>
         [DataMember]
-        [JsonProperty("alterarDataVencimento")]
-        public bool AlterarDataVencimento { get; set; }
+        [JsonProperty("exibirNotaPromissoria")]
+        public bool ExibirNotaPromissoria { get; set; }
 
         /// <summary>
-        /// Obtém ou define um valor que indica se será permitido cancelar a reapresentação do cheque.
+        /// Obtém ou define um valor que indica se o item possui log de cancelamento.
         /// </summary>
         [DataMember]
-        [JsonProperty("cancelarReapresentacao")]
-        public bool CancelarReapresentacao { get; set; }
-
-        /// <summary>
-        /// Obtém ou define um valor que indica se será permitido cancelar a devolução do cheque.
-        /// </summary>
-        [DataMember]
-        [JsonProperty("cancelarDevolucao")]
-        public bool CancelarDevolucao { get; set; }
-
-        /// <summary>
-        /// Obtém ou define um valor que indica se será permitido cancelar o protesto do cheque.
-        /// </summary>
-        [DataMember]
-        [JsonProperty("cancelarProtesto")]
-        public bool CancelarProtesto { get; set; }
-
-        /// <summary>
-        /// Obtém ou define um valor que indica se o cheque possui log de alterações.
-        /// </summary>
-        [DataMember]
-        [JsonProperty("logAlteracoes")]
-        public bool LogAlteracoes { get; set; }
+        [JsonProperty("logCancelamento")]
+        public bool LogCancelamento { get; set; }
     }
 }
--- Api/Glass.API.Backend/Models/Cheques/Lista/PermissoesDto.cs
+++ Api/Glass.API.Backend/Models/Cheques/Lista/PermissoesDto.cs
@@ -5,33 +5,54 @@
 using Newtonsoft.Json;
 using System.Runtime.Serialization;
 
-namespace Glass.API.Backend.Models.Acertos.Lista
+namespace Glass.API.Backend.Models.Cheques.Lista
 {
     /// <summary>
-    /// Classe que encapsula os dados de permissão da tela de listagem de acertos.
+    /// Classe que encapsula os dados de permissão da tela de cheques.
     /// </summary>
     [DataContract(Name = "Permissoes")]
     public class PermissoesDto
     {
         /// <summary>
-        /// Obtém ou define um valor que indica se será permitido cancelar o acerto.
+        /// Obtém ou define um valor que indica se será permitido alterar dados do cheque.
         /// </summary>
         [DataMember]
-        [JsonProperty("cancelar")]
-        public bool Cancelar { get; set; }
+        [JsonProperty("alterarDadosCheque")]
+        public bool AlterarDadosCheque { get; set; }
 
         /// <summary>
-        /// Obtém ou define um valor que indica se será permitido imprimir a nota promissória do acerto.
+        /// Obtém ou define um valor que indica se será permitido alterar a data de vencimento do cheque.
         /// </summary>
         [DataMember]
-        [JsonProperty("exibirNotaPromissoria")]
-        public bool ExibirNotaPromissoria { get; set; }
+        [JsonProperty("alterarDataVencimento")]
+        public bool AlterarDataVencimento { get; set; }
 
         /// <summary>
-        /// Obtém ou define um valor que indica se o item possui log de cancelamento.
+        /// Obtém ou define um valor que indica se será permitido cancelar a reapresentação do cheque.
         /// </summary>
         [DataMember]
-        [JsonProperty("logCancelamento")]
-        public bool LogCancelamento { get; set; }
+        [JsonProperty("cancelarReapresentacao")]
+        public bool CancelarReapresentacao { get; set; }
+
+        /// <summary>
+        /// Obtém ou define um valor que indica se será permitido cancelar a devolução do cheque.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("cancelarDevolucao")]
+        public bool CancelarDevolucao { get; set; }
+
+        /// <summary>
+        /// Obtém ou define um valor que indica se será permitido cancelar o protesto do cheque.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("cancelarProtesto")]
+        public bool CancelarProtesto { get; set; }
+
+        /// <summary>
+        /// Obtém ou define um valor que indica se o cheque possui log de alterações.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("logAlteracoes")]
+        public bool LogAlteracoes { get; set; }
     }
 }
