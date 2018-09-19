7386424550521b4cbda437ed37bff82ed620b80e
André <andre@syncsoftwares.com.br>
2018-09-19 16:30:53 -0300

    Implementação da API de sinais recebidos
    Related Work Items: #762

---
+++ Api/Glass.API.Backend/Controllers/Sinais/V1/SinaisController.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="SinaisController.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+using System.Web.Http;
+
+namespace Glass.API.Backend.Controllers.Sinais.V1
+{
+    /// <summary>
+    /// Controller de sinais.
+    /// </summary>
+    [RoutePrefix("api/v1/sinais")]
+    public partial class SinaisController : BaseController
+    {
+    }
+}
