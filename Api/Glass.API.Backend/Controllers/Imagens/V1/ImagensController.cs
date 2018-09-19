983c514f8e50528c2462608c114eb72ad9e119c5
Johan Felipe de Freitas Ribeiro <johan@syncsoftwares.com.br>
2018-09-19 20:24:50 +0000

    Merged PR 98: Criação do controle de popup
    - #778 Criação do controle de exibição de imagens em popup
    
    Inclui correções para funcionamento no ambiente de desenvolvimento.
    Inclui realocação do controller de transportadores para manter o padrão estrutural da API.
    - Merge remote-tracking branch 'origin/master' into feature/778-criar-controle-imagem-popup
    
    Related work items: #778

---
+++ Api/Glass.API.Backend/Controllers/Imagens/V1/ImagensController.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="ImagensController.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+using Glass.API.Backend.Models.Imagens.Exibicao;
+using System.Web.Http;
+
+namespace Glass.API.Backend.Controllers.Imagens.V1
+{
+    /// <summary>
+    /// Controller para recuperação de dados de imagens.
+    /// </summary>
+    [RoutePrefix("api/v1/imagens")]
+    public partial class ImagensController : BaseController
+    {
+        private IHttpActionResult ValidarIdItem(int id)
+        {
+            if (id <= 0)
+            {
+                return this.ErroValidacao("O identificador do item deve ser maior que 0.");
+            }
+
+            return null;
+        }
+
+        private IHttpActionResult ValidarDadosItem(int id, TipoItem? tipo)
+        {
+            var validacao = this.ValidarIdItem(id);
+
+            if (validacao != null)
+            {
+                return validacao;
+            }
+
+            if (!tipo.HasValue)
+            {
+                return this.ErroValidacao("Tipo de item inválido.");
+            }
+
+            return null;
+        }
+    }
+}
