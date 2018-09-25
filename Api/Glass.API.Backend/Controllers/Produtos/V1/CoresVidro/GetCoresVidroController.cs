e86ed17aac442fbf99ee4b4cb86862826aa76fd0
André <andre@syncsoftwares.com.br>
2018-09-25 07:59:07 -0300

    Criação da API
    Related Work Items: #742

--- Api/Glass.API.Backend/Controllers/Produtos/V1/CoresVidro/GetCoresVidroController.cs
+++ Api/Glass.API.Backend/Controllers/Produtos/V1/CoresVidro/GetCoresVidroController.cs
@@ -4,6 +4,7 @@
 
 using GDA;
 using Glass.API.Backend.Models.Genericas;
+using Glass.API.Backend.Models.Produtos.CoresVidro.Lista;
 using Glass.Data.DAL;
 using Swashbuckle.Swagger.Annotations;
 using System.Collections.Generic;
@@ -17,6 +18,38 @@ namespace Glass.API.Backend.Controllers.Produtos.V1.CoresVidro
     /// </summary>
     public partial class CoresVidroController : BaseController
     {
+        /// <summary>
+        /// Recupera a lista de cores de vidro para a tela de listagem.
+        /// </summary>
+        /// <param name="filtro">O filtro para a busca de cores de vidro.</param>
+        /// <returns>Uma lista JSON com os dados das cores de vidro.</returns>
+        [HttpGet]
+        [Route("")]
+        [SwaggerResponse(200, "Cores de vidro encontradas sem paginação (apenas uma página de retorno) ou última página retornada.", Type = typeof(IEnumerable<ListaDto>))]
+        [SwaggerResponse(204, "Cores de vidro não encontradas para o filtro informado.")]
+        [SwaggerResponse(206, "Cores de vidro paginados (qualquer página, exceto a última).", Type = typeof(IEnumerable<ListaDto>))]
+        public IHttpActionResult ObterLista([FromUri] FiltroDto filtro)
+        {
+            using (var sessao = new GDATransaction())
+            {
+                var fluxo = Microsoft.Practices.ServiceLocation.ServiceLocator
+                    .Current.GetInstance<Global.Negocios.ICoresFluxo>();
+
+                var coresVidro = fluxo.PesquisarCoresVidro();
+
+                ((Colosoft.Collections.IVirtualList)coresVidro).Configure(filtro.NumeroRegistros);
+                ((Colosoft.Collections.ISortableCollection)coresVidro).ApplySort(filtro.ObterTraducaoOrdenacao());
+
+                return this.ListaPaginada(
+                    coresVidro
+                        .Skip(filtro.ObterPrimeiroRegistroRetornar())
+                        .Take(filtro.NumeroRegistros)
+                        .Select(entidade => new ListaDto(entidade)),
+                    filtro,
+                    () => coresVidro.Count);
+            }
+        }
+
         /// <summary>
         /// Recupera as cores de vidro para os controles de filtro das telas.
         /// </summary>
---
+++ Api/Glass.API.Backend/Controllers/Produtos/V1/CoresVidro/PostCoresVidroController.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="PostCoresVidroController.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+using GDA;
+using Glass.API.Backend.Helper.Produtos.CoresVidro;
+using Glass.API.Backend.Helper.Respostas;
+using Glass.API.Backend.Models.Produtos.CoresVidro.CadastroAtualizacao;
+using Glass.Data.DAL;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Web.Http;
+
+namespace Glass.API.Backend.Controllers.Produtos.V1.CoresVidro
+{
+    /// <summary>
+    /// Controller de cores de Vidro.
+    /// </summary>
+    public partial class CoresVidroController : BaseController
+    {
+        /// <summary>
+        /// Cadastra uma cor de Vidro.
+        /// </summary>
+        /// <param name="dadosParaCadastro">Objeto contendo dados para inserção de uma cor de Vidro.</param>
+        /// <returns>O status HTTP que representa o resultado da operação.</returns>
+        [HttpPost]
+        [Route("")]
+        [SwaggerResponse(201, "Cor de Vidro cadastrada.", Type = typeof(CriadoDto<int>))]
+        [SwaggerResponse(400, "Erro de validação.", Type = typeof(MensagemDto))]
+        public IHttpActionResult CadastrarCorVidro([FromBody] CadastroAtualizacaoDto dadosParaCadastro)
+        {
+            using (var sessao = new GDATransaction())
+            {
+                try
+                {
+                    var corVidro = new ConverterCadastroAtualizacaoParaCorVidro(dadosParaCadastro)
+                        .ConverterParaCorVidro();
+
+                    var idCorVidro = CorVidroDAO.Instance.Insert(sessao, corVidro);
+                    sessao.Commit();
+
+                    return this.Criado("Cor de Vidro inserida com sucesso!", idCorVidro);
+                }
+                catch (Exception ex)
+                {
+                    sessao.Rollback();
+                    return this.ErroValidacao($"Erro ao inserir cor de Vidro.", ex);
+                }
+            }
+        }
+    }
+}
