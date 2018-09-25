0406d0a376c91dcf605031ef15139aa9e8b25f1c
André <andre@syncsoftwares.com.br>
2018-09-25 07:32:38 -0300

    Implementação da API
    Related Work Items: #767

--- Api/Glass.API.Backend/Controllers/Produtos/V1/CoresAluminio/GetCoresAluminioController.cs
+++ Api/Glass.API.Backend/Controllers/Produtos/V1/CoresAluminio/GetCoresAluminioController.cs
@@ -18,6 +18,38 @@ namespace Glass.API.Backend.Controllers.Produtos.V1.CoresAluminio
     public partial class CoresAluminioController : BaseController
     {
         /// <summary>
+        /// Recupera a lista de cores de alumínio para a tela de listagem.
+        /// </summary>
+        /// <param name="filtro">O filtro para a busca de cores de alumínio.</param>
+        /// <returns>Uma lista JSON com os dados das cores de alumínio.</returns>
+        [HttpGet]
+        [Route("")]
+        [SwaggerResponse(200, "Cores de alumínio encontradas sem paginação (apenas uma página de retorno) ou última página retornada.", Type = typeof(IEnumerable<ListaDto>))]
+        [SwaggerResponse(204, "Cores de alumínio não encontradas para o filtro informado.")]
+        [SwaggerResponse(206, "Cores de alumínio paginados (qualquer página, exceto a última).", Type = typeof(IEnumerable<ListaDto>))]
+        public IHttpActionResult ObterLista([FromUri] FiltroDto filtro)
+        {
+            using (var sessao = new GDATransaction())
+            {
+                var fluxo = Microsoft.Practices.ServiceLocation.ServiceLocator
+                    .Current.GetInstance<Global.Negocios.ICoresFluxo>();
+
+                var coresAluminio = fluxo.PesquisarCoresAluminio();
+
+                ((Colosoft.Collections.IVirtualList)coresAluminio).Configure(filtro.NumeroRegistros);
+                ((Colosoft.Collections.ISortableCollection)coresAluminio).ApplySort(filtro.ObterTraducaoOrdenacao());
+
+                return this.ListaPaginada(
+                    coresAluminio
+                        .Skip(filtro.ObterPrimeiroRegistroRetornar())
+                        .Take(filtro.NumeroRegistros)
+                        .Select(entidade => new ListaDto(entidade)),
+                    filtro,
+                    () => coresAluminio.Count);
+            }
+        }
+
+        /// <summary>
         /// Recupera as cores de alumínio para os controles de filtro das telas.
         /// </summary>
         /// <returns>Uma lista JSON com as cores de alumínio encontradas.</returns>
---
+++ Api/Glass.API.Backend/Models/Produtos/CoresAluminio/Lista/FiltroDto.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="FiltroDto.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+using Glass.API.Backend.Helper.Produtos.CoresAluminio;
+using Glass.API.Backend.Models.Genericas;
+
+namespace Glass.API.Backend.Models.Produtos.CoresAluminio.Lista
+{
+    /// <summary>
+    /// Classe que encapsula os itens de filtro para a lista de cores de alumínio.
+    /// </summary>
+    public class FiltroDto : PaginacaoDto
+    {
+        /// <summary>
+        /// Inicia uma nova instância da classe <see cref="FiltroDto"/>.
+        /// </summary>
+        public FiltroDto()
+            : base(item => new TraducaoOrdenacaoListaCoresAluminio(item.Ordenacao))
+        {
+        }
+    }
+}
