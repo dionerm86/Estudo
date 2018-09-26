d9e317d6f911f41f285a1aca2e62d0a108f25f5d
André <andre@syncsoftwares.com.br>
2018-09-26 14:11:40 -0300

    Desenvolvimento da API
    Related Work Items: #816

---
+++ Api/Glass.API.Backend/Helper/Comissionados/TraducaoOrdenacaoListaComissionados.cs
@@ -0,0 +1,55 @@
+﻿// <copyright file="TraducaoOrdenacaoListaComissionados.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+namespace Glass.API.Backend.Helper.Comissionados
+{
+    /// <summary>
+    /// Classe que realiza a tradução dos campos de ordenação para a lista de comissionados.
+    /// </summary>
+    internal class TraducaoOrdenacaoListaComissionados : BaseTraducaoOrdenacao
+    {
+        /// <summary>
+        /// Inicia uma nova instância da classe <see cref="TraducaoOrdenacaoListaComissionados"/>.
+        /// </summary>
+        /// <param name="ordenacao">A ordenação realizada na tela.</param>
+        public TraducaoOrdenacaoListaComissionados(string ordenacao)
+            : base(ordenacao)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override string OrdenacaoPadrao
+        {
+            get { return "IdComissionado ASC"; }
+        }
+
+        /// <inheritdoc/>
+        protected override string TraduzirCampo(string campo)
+        {
+            switch (campo.ToLowerInvariant())
+            {
+                case "id":
+                    return "IdComissionado";
+
+                case "nome":
+                    return "Nome";
+
+                case "cpfcnpj":
+                    return "CpfCnpj";
+
+                case "rginscricaoestadual":
+                    return "RgInscEst";
+
+                case "telefoneresidencial":
+                    return "telres";
+
+                case "telefonecelular":
+                    return "telcel";
+
+                default:
+                    return this.OrdenacaoPadrao;
+            }
+        }
+    }
+}
---
+++ Api/Glass.API.Backend/Models/Comissionados/Lista/ListaDto.cs
@@ -0,0 +1,67 @@
+﻿// <copyright file="ListaDto.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+using Glass.API.Backend.Models.Genericas;
+using Glass.Data.DAL;
+using Glass.Data.Model;
+using Glass.Global.Negocios.Entidades;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace Glass.API.Backend.Models.Comissionados.Lista
+{
+    /// <summary>
+    /// Classe que encapsula os dados de um comissionado para a tela de listagem.
+    /// </summary>
+    [DataContract(Name = "Comissionado")]
+    public class ListaDto : IdNomeDto
+    {
+        /// <summary>
+        /// Inicia uma nova instância da classe <see cref="ListaDto"/>.
+        /// </summary>
+        /// <param name="comissionado">A model de comissionado.</param>
+        internal ListaDto(Global.Negocios.Entidades.Comissionado comissionado)
+        {
+            this.Id = comissionado.IdComissionado;
+            this.Nome = comissionado.Nome;
+            this.CpfCnpj = comissionado.CpfCnpj;
+            this.RgInscricaoEstadual = comissionado.RgInscEst;
+            this.Percentual = (decimal)comissionado.Percentual;
+
+            this.Telefones = new TelefonesDto
+            {
+                Residencial = comissionado.TelRes,
+                Celular = comissionado.TelCel,
+            };
+        }
+
+        /// <summary>
+        /// Obtém ou define o CFP/CNPJ do comissionado.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("cpfCnpj")]
+        public string CpfCnpj { get; set; }
+
+        /// <summary>
+        /// Obtém ou define o RG/Inscrição estadual do comissionado.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("rgInscricaoEstadual")]
+        public string RgInscricaoEstadual { get; set; }
+
+        /// <summary>
+        /// Obtém ou define o percentual de comissão do comissionado.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("percentual")]
+        public decimal Percentual { get; set; }
+
+        /// <summary>
+        /// Obtém ou define os telefones do comissionado.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("telefones")]
+        public TelefonesDto Telefones { get; set; }
+    }
+}
