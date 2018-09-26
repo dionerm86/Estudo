6007dcb3e1228a2829ceecd9023199b706729f55
André <andre@syncsoftwares.com.br>
2018-09-26 17:07:15 -0300

    Implementação da API
    Related Work Items: #822

---
+++ Api/Glass.API.Backend/Controllers/PlanosConta/V1/PlanosContaController.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="ParcelasController.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+using System.Web.Http;
+
+namespace Glass.API.Backend.Controllers.Parcelas.V1
+{
+    /// <summary>
+    /// Controller de parcelas.
+    /// </summary>
+    [RoutePrefix("api/v1/parcelas")]
+    public partial class ParcelasController : BaseController
+    {
+    }
+}
