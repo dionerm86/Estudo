5ca0272a5f5f07e5312e6a7eb0231db7dd43f179
Johan Felipe de Freitas Ribeiro <johan@syncsoftwares.com.br>
2018-10-05 10:13:40 -0300

    #850 Finalização do controle de beneficiamentos por setor

--- Glass/Vue/_Compartilhado/Componentes/ControleBeneficiamentosSetor.js
+++ Glass/Vue/_Compartilhado/Componentes/ControleBeneficiamentosSetor.js
@@ -1,6 +1,10 @@
 ﻿Vue.component('controle-beneficiamentos-setor', {
   mixins: [Mixins.UUID, Mixins.Beneficiamentos, Mixins.Objetos],
   props: {
+    /**
+     * Identificadores dos beneficiamentos selecionados.
+     * @type {number[]}
+     */
     idsSelecionados: {
       required: true,
       twoWay: true,
@@ -22,8 +26,7 @@
       uuid: null,
       estiloGridPrincipal: estiloGrid(3),
       estiloGridFilhos: estiloGrid(4),
-      selecionados: [],
-      filhosSelecionados: []
+      pais: []
     };
   },
 
@@ -36,6 +39,15 @@
      */
     idUnico: function(beneficiamento, complemento) {
       return this.uuid + '_' + beneficiamento.id + (complemento ? '_' + complemento : '');
+    },
+
+    /**
+     * Retorna um valor que indica se o beneficiamento possui filhos.
+     * @param {object} beneficiamento O beneficiamento que está sendo verificado.
+     * @returns {boolean} Verdadeiro, se o beneficiamento possuir filhos.
+     */
+    temFilhos: function(beneficiamento) {
+      return beneficiamento && beneficiamento.filhos && beneficiamento.filhos.length;
     }
   },
 
@@ -44,52 +56,47 @@
   },
 
   computed: {
+    /**
+     * Propriedade computada que retorna os identificadores dos beneficiamentos selecionados
+     * de forma normalizada e que atualiza a propriedade principal em caso de alteração.
+     */
     idsSelecionadosAtuais: {
       get: function () {
-        var idsPais = this.selecionados.slice().filter(function (id) {
-          var beneficiamento = this.beneficiamentos.find(function (item) {
-            return item.id === id;
-          });
-
-          return beneficiamento && !(beneficiamento.filhos && beneficiamento.filhos.length);
-        }, this);
-
-        var idsFilhos = this.filhosSelecionados.slice();
-
-        return idsPais.concat(idsFilhos);
+        return this.idsSelecionados || [];
       },
       set: function (valor) {
-        if (!this.equivalentes(valor, this.idsSelecionadosAtuais)) {
+        if (!this.equivalentes(valor, this.idsSelecionados)) {
           this.$emit('update:idsSelecionados', valor);
 
           if (valor && valor.length) {
-            var pais = new Set(valor.filter(function (id) {
-              var beneficiamento = this.beneficiamentos.find(function (item) {
-                if (item.id === id) {
-                  return true;
-                }
+            var pais = new Set(valor.concat(this.pais)
+              .map(function (id) {
+                var beneficiamento = this.beneficiamentos.find(function (item) {
+                  if (item.id === id) {
+                    return true;
+                  }
 
-                if (item.filhos && item.filhos.length) {
-                  for (var filho of item.filhos) {
-                    if (filho.id === id) {
-                      return true;
+                  if (item.filhos && item.filhos.length) {
+                    for (var filho of item.filhos) {
+                      if (filho.id === id) {
+                        return true;
+                      }
                     }
                   }
-                }
 
-                return false;
-              });
+                  return false;
+                });
 
-              return !!beneficiamento;
-            }, this));
+                return beneficiamento ? beneficiamento.id : null;
+              }, this)
+              .filter(function (id) {
+                return id;
+              })
+            );
 
-            this.selecionados = [...pais];
-            this.filhosSelecionados = valor.filter(function (id) {
-              return this.selecionados.indexOf(id) === -1;
-            }, this);
+            this.pais = [...pais];
           } else {
-            this.selecionados = [];
-            this.filhosSelecionados = [];
+            this.pais = [];
           }
         }
       }
@@ -97,9 +104,13 @@
   },
 
   watch: {
-    selecionados: {
+    /**
+     * Observador para a variável 'pais'.
+     * Atualiza a lista de beneficiamentos selecionados se houver remoção de um pai.
+     */
+    pais: {
       handler: function (atual, anterior) {
-        var removido = anterior.filter(function (item) {
+        var removido = anterior.find(function (item) {
           return atual.indexOf(item) === -1;
         });
 
@@ -107,21 +118,19 @@
           return;
         }
 
-        var beneficiamento = this.beneficiamentos.find(function (item) {
-          return item.id === removido;
+        var filhosBeneficiamento = this.beneficiamentos
+          .find(function (item) {
+            return item.id === removido;
+          })
+          .filhos || [];
+
+        filhosBeneficiamento = filhosBeneficiamento.map(function (item) {
+          return item.id;
         });
 
-        if (beneficiamento && beneficiamento.filhos && beneficiamento.filhos.length) {
-          var idsFilhos = beneficiamento.filhos.map(function (item) {
-            return item.id;
-          });
-
-          this.filhosSelecionados = this.filhosSelecionados
-            .slice()
-            .filter(function (item) {
-              return idsFilhos.indexOf(item.id) === -1;
-            });
-        }
+        this.idsSelecionados = this.idsSelecionados.filter(function (id) {
+          return filhosBeneficiamento.indexOf(id) === -1;
+        });
       },
       deep: true
     }
