e340cd6c087353a7e2a464bdeb42cdeea1fcffd6
Felipe Ribeiro <felipe@syncsoftwares.com.br>
2018-10-09 17:17:44 -0300

    #81324 Criação de Condutor para Mdfe

--- Glass.Data/Fiscal/DAL/CondutoresDAO.cs
+++ Glass.Data/Fiscal/DAL/CondutoresDAO.cs
@@ -18,7 +18,9 @@ namespace Glass.Data.DAL
         public override uint Insert(GDASession sessao, Condutores objInsert)
         {
             if (objPersistence.ExecuteSqlQueryCount(sessao, $"SELECT * FROM condutores WHERE CPF=?cpf", new GDA.GDAParameter("?cpf", objInsert.Cpf)) > 0)
+            {
                 throw new System.Exception("Já existe um condutor cadastrado com o mesmo Cpf");
+            }
 
             return base.Insert(sessao, objInsert);
         }
@@ -27,9 +29,22 @@ namespace Glass.Data.DAL
         {
             if (objPersistence.ExecuteSqlQueryCount(sessao, $"SELECT * FROM condutores WHERE IDCONDUTOR!=?idCondutor AND CPF=?cpf",
                 new GDA.GDAParameter("?idCondutor", objUpdate.IdCondutor), new GDA.GDAParameter("?cpf", objUpdate.Cpf)) > 0)
+            {
                 throw new System.Exception("Já existe um condutor cadastrado com o mesmo Cpf");
+            }
 
             return base.Update(sessao, objUpdate);
+        }
+
+        public override int DeleteByPrimaryKey(GDASession sessao, int idCondutor)
+        {
+            if (objPersistence.ExecuteSqlQueryCount(sessao, $"SELECT * FROM condutor_veiculo_mdfe WHERE IDCONDUTOR!=?idCondutor",
+                new GDA.GDAParameter("?idCondutor", idCondutor)) > 0)
+            {
+                throw new System.Exception("O Condutor está associado a um MDFe.");
+            }
+
+            return base.DeleteByPrimaryKey(sessao, idCondutor);
         }
     }
 }
