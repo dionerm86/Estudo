78d851d01fd39311d3a76df07d99af70e3fe6f02
Felipe Ribeiro <felipe@syncsoftwares.com.br>
2018-10-11 10:28:47 -0300

    #80087 Inserção de totais de vendas para o cliente na consulta rápida do cliente.

--- Glass/Listas/LstConsultaRapidaCliente.aspx.cs
+++ Glass/Listas/LstConsultaRapidaCliente.aspx.cs
@@ -7,6 +7,8 @@ using Glass.Data.DAL;
 using System.Drawing;
 using Glass.Configuracoes;
 using Colosoft;
+using System.Data;
+using System.Linq;
 
 namespace Glass.UI.Web.Listas
 {
@@ -16,37 +18,37 @@ namespace Glass.UI.Web.Listas
         {
             Ajax.Utility.RegisterTypeForAjax(typeof(Glass.UI.Web.Listas.LstConsultaRapidaCliente));
             Ajax.Utility.RegisterTypeForAjax(typeof(MetodosAjax));
-    
+
             if (string.IsNullOrEmpty(txtNumCli.Text))
             {
                 tblInfo.Visible = false;
                 tblTabelas.Visible = false;
             }
-    
+
             lblPedidosBloqueio.Text = "Pedidos prontos porém não liberados a " + PedidoConfig.NumeroDiasPedidoProntoAtrasado + " dias";
         }
-    
+
         protected void btnBuscar_Click(object sender, EventArgs e)
         {
             var idCliente = Glass.Conversoes.StrParaUint(txtNumCli.Text);
-    
+
             if (idCliente == 0)
                  return;
-     
+
             lblTotalDebitos.Text = ContasReceberDAO.Instance.ObtemTotalDebitosCliente(idCliente).ToString("C");
-    
+
             BuscaInfoCliente(idCliente);
-    
+
             tblInfo.Visible = true;
             tblTabelas.Visible = true;
         }
-    
+
         protected void BuscaInfoCliente(uint idCli)
         {
             Cliente cli = ClienteDAO.Instance.GetElement(idCli);
-    
+
             #region Dados Cadastrais
-    
+
             lblInfoCodCli.Text = cli.IdCli.ToString();
             lblInfoNomeCli.Text = cli.Nome;
             lblInfoContato.Text = ((!string.IsNullOrEmpty(cli.Contato) ? cli.Contato : null) +
@@ -71,19 +73,19 @@ namespace Glass.UI.Web.Listas
 
             if (cli.IdFunc > 0)
                 lblInfoVendedor.Text = FuncionarioDAO.Instance.GetNome((uint)cli.IdFunc.Value);
-    
+
             lblInfoSituacao.Text = cli.DescrSituacao;
-    
+
             if (cli.Situacao > 1)
             {
                 lblInfoSituacao.ForeColor = Color.Red;
                 lblInfoSituacao.Font.Bold = true;
             }
-    
+
             #endregion
-    
+
             #region Dados Financeiros
-    
+
             lblFinancLimPadrao.Text = cli.Limite.ToString("C");
             lblPagarAntesProducao.Text = cli.PagamentoAntesProducao ? "Sim" : "Não";
             lblTabelaDescontoAcrescimo.Text = cli.TabelaDescontoAcrescimo;
@@ -97,12 +99,12 @@ namespace Glass.UI.Web.Listas
             if (formasPagtoDisp.Count > 0)
             {
                 lblFinancPagtoDisp.Text = "<ul>";
-    
+
                 foreach (FormaPagto fp in formasPagtoDisp)
                 {
                     lblFinancPagtoDisp.Text += "<li>" + fp.Descricao + "</li>";
                 }
-                        
+
                 lblFinancPagtoDisp.Text += "</ul>";
             }
 
@@ -113,29 +115,31 @@ namespace Glass.UI.Web.Listas
             if (parcelasDisp.Length > 0)
             {
                 lblFinancParcDisp.Text = "<ul>";
-    
+
                 foreach (Parcelas p in parcelasDisp)
                 {
-                    if(!p.NaoUsar && p.Situacao == Situacao.Ativo) 
+                    if(!p.NaoUsar && p.Situacao == Situacao.Ativo)
                         lblFinancParcDisp.Text += "<li>" + p.DescrCompleta + "</li>";
                 }
-    
+
                 lblFinancParcDisp.Text += "</ul>";
             }
-    
+
             lblFinancPercSinalMin.Text = cli.PercSinalMinimo == null ? "" : cli.PercSinalMinimo.ToString() + "%";
             lblFinancCredito.Text = cli.Credito.ToString("C");
 
             lblContasVencidas.Text = ContasReceberDAO.Instance.ClientePossuiContasVencidas((uint)cli.IdCli) ? "Cliente Possui Contas Vencidas" : "";
 
+            CarregarGrdVendas(idCli);
+
             #endregion
         }
-    
+
         protected void imgPesq_click(object sender, ImageClickEventArgs e)
         {
             grdSugestao.PageIndex = 0;
         }
-    
+
         [Ajax.AjaxMethod()]
         public static string GetCli(string idCli)
         {
@@ -144,19 +148,19 @@ namespace Glass.UI.Web.Listas
             else
                 return "Ok;" + ClienteDAO.Instance.GetNome(Glass.Conversoes.StrParaUint(idCli));
         }
-    
+
         protected void grdLimiteCliente_DataBound(object sender, EventArgs e)
         {
         }
-    
+
         protected void grdDescontoAcrescimo_RowDataBound(object sender, GridViewRowEventArgs e)
         {
             if (e.Row.RowType != DataControlRowType.DataRow)
                 return;
-    
+
             e.Row.FindControl("grdLimiteCliente").DataBind();
         }
-    
+
         protected void grdSugestao_RowCommand(object sender, GridViewCommandEventArgs e)
         {
             if (e.CommandName == "Cancelar")
@@ -165,12 +169,13 @@ namespace Glass.UI.Web.Listas
                 grdSugestao.DataBind();
             }
         }
+
         protected void grdSugestao_RowDataBound(object sender, GridViewRowEventArgs e)
         {
             if (e.Row.RowType == DataControlRowType.DataRow)
             {
                 var excluir = (LinkButton)e.Row.FindControl("lnkExcluir");
-    
+
                 if (UserInfo.GetUserInfo.IsAdministrador)
                 {
                     excluir.Visible = true;
@@ -181,6 +186,56 @@ namespace Glass.UI.Web.Listas
                 }
             }
         }
-    
+
+        /// <summary>
+        /// Recupera os dados para a grid de compras do cliente.
+        /// </summary>
+        /// <param name="idCliente"></param>
+        protected void CarregarGrdVendas(uint idCliente)
+        {
+            var dataAtual = DateTime.Now;
+
+            int mesInicio = dataAtual.AddMonths(-5).Month;
+            int mesFim = dataAtual.Month;
+            int anoInicio = dataAtual.AddMonths(-5).Year;
+            int anoFim = dataAtual.Year;
+
+            var vendasCliente = Data.RelDAL.VendasDAO.Instance.GetList(idCliente, null, null, false, 0, null,
+                mesInicio, anoInicio, mesFim, anoFim, 0, null, 0, null, null, null, 0, 0, 0, 0, 0, false, null, 0, null)
+                .FirstOrDefault();
+
+            if (vendasCliente == null)
+            {
+                return;
+            }
+
+            var tabela = new DataTable();
+
+            foreach (string mesAno in vendasCliente.MesVenda)
+            {
+                tabela.Columns.Add(new DataColumn(mesAno)
+                {
+                    DataType = typeof(string),
+                });
+            }
+
+            var linha = tabela.NewRow();
+
+            for (int i = 0; i < tabela.Columns.Count; i++)
+            {
+                linha[i] = string.Format("{0:C}", vendasCliente.ValorVenda[i]);
+            }
+
+            tabela.Columns.Add(new DataColumn("Total"));
+
+            linha[tabela.Columns.Count - 1] = string.Format("{0:C}", vendasCliente.Total);
+
+            tabela.Rows.Add(linha);
+
+            grdVendas.DataSource = tabela;
+
+            grdVendas.DataBind();
+        }
+
     }
 }
