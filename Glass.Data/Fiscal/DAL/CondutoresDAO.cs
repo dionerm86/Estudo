ad58a2e43f53a5f3be169101616e8dddefec0490
Felipe Ribeiro <felipe@syncsoftwares.com.br>
2018-10-15 15:14:31 -0300

    #81324 Criação de Condutor para Mdfe

--- Glass.Data/Fiscal/DAL/CondutoresDAO.cs
+++ Glass.Data/Fiscal/DAL/CondutoresDAO.cs
@@ -10,41 +10,95 @@ namespace Glass.Data.DAL
         /// <summary>
         /// Recupera a listagem de condutores.
         /// </summary>
+        /// <returns>Lista de condutores.</returns>
         public IList<Condutores> GetList()
         {
             return objPersistence.LoadData("select * from condutores").ToList();
         }
 
-        public override uint Insert(GDASession sessao, Condutores objInsert)
+        /// <summary>
+        /// Verificar se o condutor pode ser inserido.
+        /// </summary>
+        /// <param name="session">Sessão.</param>
+        /// <param name="cpf">Cpf do condutor.</param>
+        private void VerificarPodeInserirCondutor(GDASession session, string cpf)
         {
-            if (objPersistence.ExecuteSqlQueryCount(sessao, $"SELECT * FROM condutores WHERE CPF=?cpf", new GDA.GDAParameter("?cpf", objInsert.Cpf)) > 0)
+            if (objPersistence.ExecuteSqlQueryCount(session, $"SELECT * FROM condutores WHERE CPF=?cpf", new GDAParameter("?cpf", cpf)) > 0)
             {
                 throw new System.Exception("Já existe um condutor cadastrado com o mesmo Cpf");
             }
-
-            return base.Insert(sessao, objInsert);
         }
 
-        public override int Update(GDASession sessao, Condutores objUpdate)
+        /// <summary>
+        /// Insere um novo condutor.
+        /// </summary>
+        /// <param name="session">Sessão.</param>
+        /// <param name="objInsert">Novo condutor.</param>
+        /// <returns>Identificador do condutor inserido.</returns>
+        public override uint Insert(GDASession session, Condutores objInsert)
         {
-            if (objPersistence.ExecuteSqlQueryCount(sessao, $"SELECT * FROM condutores WHERE IDCONDUTOR!=?idCondutor AND CPF=?cpf",
-                new GDA.GDAParameter("?idCondutor", objUpdate.IdCondutor), new GDA.GDAParameter("?cpf", objUpdate.Cpf)) > 0)
+            VerificarPodeInserirCondutor(session, objInsert.Cpf);
+
+            return base.Insert(session, objInsert);
+        }
+
+        /// <summary>
+        /// Verifica se o condutor poderá ser alterados com os dados selecionados.
+        /// </summary>
+        /// <param name="session">Sessão.</param>
+        /// <param name="condutor">Condutor cujos dados serão verificados.</param>
+        private void VerificarPodeAtualizarCondutor(GDASession session, Condutores condutor)
+        {
+            if (objPersistence.ExecuteSqlQueryCount(
+                session,
+                $"SELECT * FROM condutores WHERE IDCONDUTOR!=?idCondutor AND CPF=?cpf",
+                new GDAParameter("?idCondutor", condutor.IdCondutor),
+                new GDAParameter("?cpf", condutor.Cpf)) > 0)
             {
                 throw new System.Exception("Já existe um condutor cadastrado com o mesmo Cpf");
             }
-
-            return base.Update(sessao, objUpdate);
         }
 
-        public override int DeleteByPrimaryKey(GDASession sessao, int idCondutor)
+        /// <summary>
+        /// Método para alteração de um condutor.
+        /// </summary>
+        /// <param name="session">Sessão.</param>
+        /// <param name="objUpdate">Condutor que está sendo alterado.</param>
+        /// <returns>>Retorna o identificador do condutor inserido.</returns>
+        public override int Update(GDASession session, Condutores objUpdate)
         {
-            if (objPersistence.ExecuteSqlQueryCount(sessao, $"SELECT * FROM condutor_veiculo_mdfe WHERE IDCONDUTOR!=?idCondutor",
-                new GDA.GDAParameter("?idCondutor", idCondutor)) > 0)
+            VerificarPodeAtualizarCondutor(session, objUpdate);
+
+            return base.Update(session, objUpdate);
+        }
+
+        /// <summary>
+        /// Verifica se o condutor pode ser excluído.
+        /// </summary>
+        /// <param name="sessao">Sessão.</param>
+        /// <param name="idCondutor">identificador do condutor validado.</param>
+        private void VerificarPodeExcluirCondutor(GDASession sessao, int idCondutor)
+        {
+            if (objPersistence.ExecuteSqlQueryCount(
+                sessao,
+                $"SELECT * FROM condutor_veiculo_mdfe WHERE IDCONDUTOR=?idCondutor",
+                new GDAParameter("?idCondutor", idCondutor)) > 0)
             {
                 throw new System.Exception("O Condutor está associado a um MDFe.");
             }
+        }
 
-            return base.DeleteByPrimaryKey(sessao, idCondutor);
+        /// <summary>
+        /// Deleta os dados do condutor.
+        /// </summary>
+        /// <param name="sessao">Sessão.</param>
+        /// <param name="key">Identificador do condutor.</param>
+        /// <returns>Retorna o identificador do condutor excluído.</returns>
+        public override int DeleteByPrimaryKey(GDASession sessao, int key)
+        {
+            VerificarPodeExcluirCondutor(sessao, key);
+
+            return base.DeleteByPrimaryKey(sessao, key);
         }
     }
 }
--- Glass.Data/Fiscal/Model/Condutores.cs
+++ Glass.Data/Fiscal/Model/Condutores.cs
@@ -7,12 +7,21 @@ namespace Glass.Data.Model
     [PersistenceClass("condutores")]
     public class Condutores : Colosoft.Data.BaseModel
     {
+        /// <summary>
+        /// Obtém ou define o identificador do condutor.
+        /// </summary>
         [PersistenceProperty("IDCONDUTOR", PersistenceParameterType.IdentityKey)]
         public int IdCondutor { get; set; }
 
+        /// <summary>
+        /// Obtém ou define o nome do condutor.
+        /// </summary>
         [PersistenceProperty("NOME")]
         public string Nome { get; set; }
 
+        /// <summary>
+        /// Obtém ou define O CPF do Condutor.
+        /// </summary>
         [PersistenceProperty("CPF")]
         public string Cpf { get; set; }
     }
