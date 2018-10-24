419c6b194656fbba960faf0ae827a3a1eb8c4c5b
André <andre@syncsoftwares.com.br>
2018-10-24 08:10:58 -0300

    Desenvolvimento da API da listagem de roteiros
    Related Work Items: #1123

---
+++ Api/Glass.API.Backend/Controllers/Producao/V1/Roteiros/RoteirosController.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="RoteirosController.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+using GDA;
+using Glass.Data.DAL;
+using System.Web.Http;
+
+namespace Glass.API.Backend.Controllers.Producao.V1.Roteiros
+{
+    /// <summary>
+    /// Controller de roteiros.
+    /// </summary>
+    [RoutePrefix("api/v1/producao/roteiros")]
+    public partial class RoteirosController : BaseController
+    {
+        private IHttpActionResult ValidarIdRoteiro(int id)
+        {
+            if (id <= 0)
+            {
+                return this.ErroValidacao("Identificador do roteiro deve ser um número maior que zero.");
+            }
+
+            return null;
+        }
+
+        private IHttpActionResult ValidarExistenciaIdRoteiro(GDASession sessao, int id)
+        {
+            var validacao = this.ValidarIdRoteiro(id);
+
+            if (validacao == null && !RoteiroProducaoDAO.Instance.Exists(sessao, id))
+            {
+                return this.NaoEncontrado("Roteiro não encontrado.");
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Api/Glass.API.Backend/Helper/Producao/Roteiros/TraducaoOrdenacaoListaRoteiros.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="TraducaoOrdenacaoListaRoteiros.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+namespace Glass.API.Backend.Helper.Producao.Roteiros
+{
+    /// <summary>
+    /// Classe que realiza a tradução dos campos de ordenação para a lista de roteiros.
+    /// </summary>
+    internal class TraducaoOrdenacaoListaRoteiros : BaseTraducaoOrdenacao
+    {
+        /// <summary>
+        /// Inicia uma nova instância da classe <see cref="TraducaoOrdenacaoListaRoteiros"/>.
+        /// </summary>
+        /// <param name="ordenacao">A ordenação realizada na tela.</param>
+        public TraducaoOrdenacaoListaRoteiros(string ordenacao)
+            : base(ordenacao)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override string OrdenacaoPadrao
+        {
+            get { return "CodProcesso ASC"; }
+        }
+
+        /// <inheritdoc/>
+        protected override string TraduzirCampo(string campo)
+        {
+            switch (campo.ToLowerInvariant())
+            {
+                case "processo":
+                    return "CodProcesso";
+
+                default:
+                    return this.OrdenacaoPadrao;
+            }
+        }
+    }
+}
