da56cfc3a79e43a0cf6fe605e10636d6713f75fd
Isabela Lima <isabelalima@syncsoftwares.com.br>
2019-01-23 15:09:01 -0200

    #2582-Refatoração da tela de listagem de devoluções de pagamentos

---
+++ Api/Glass.API.Backend/Models/DevolucoesPagamento/V1/Lista/ListaDto.cs
@@ -0,0 +1,86 @@
+﻿// <copyright file="ListaDto.cs" company="Sync Softwares">
+// Copyright (c) Sync Softwares. Todos os direitos reservados.
+// </copyright>
+
+using Glass.API.Backend.Models.Genericas.V1;
+using Glass.Data.DAL;
+using Glass.Data.Model;
+using Newtonsoft.Json;
+using System;
+using System.Runtime.Serialization;
+
+namespace Glass.API.Backend.Models.DevolucoesPagamento.V1.Lista
+{
+    /// <summary>
+    /// Classe que encapsula os dados da devolução do pagamento para a tela de listagem.
+    /// </summary>
+    [DataContract(Name = "Lista")]
+    public class ListaDto : IdNomeDto
+    {
+        /// <summary>
+        /// Inicia uma nova instância da classe <see cref="ListaDto"/>.
+        /// </summary>
+        /// <param name="devolucaoPagto">A model de devolução de pagamento.</param>
+        internal ListaDto(DevolucaoPagto devolucaoPagto)
+        {
+            this.Id = (int?)devolucaoPagto.IdDevolucaoPagto;
+            this.Cliente = new IdNomeDto
+            {
+                Id = (int?)devolucaoPagto.IdCliente,
+                Nome = devolucaoPagto.NomeCliente,
+            };
+
+            this.Valor = devolucaoPagto.Valor;
+            this.Situacao = devolucaoPagto.DescrSituacao;
+            this.DataCadastro = devolucaoPagto.DataCad;
+            this.UsuarioCadastro = devolucaoPagto.DescrUsuCad;
+            this.Permissoes = new PermissoesDto
+            {
+                Cancelar = devolucaoPagto.PodeCancelar,
+                LogCancelamento = LogCancelamentoDAO.Instance.TemRegistro(LogCancelamento.TabelaCancelamento.DevolucaoPagamento, devolucaoPagto.IdDevolucaoPagto),
+            };
+        }
+
+        /// <summary>
+        /// Obtém ou define o cliente referente a devolução do pagamento.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("cliente")]
+        public IdNomeDto Cliente { get; set; }
+
+        /// <summary>
+        /// Obtém ou define o valor da devolução do pagamento.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("valor")]
+        public decimal Valor { get; set; }
+
+        /// <summary>
+        /// Obtém ou define a situação da devolução do pagamento.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("situacao")]
+        public string Situacao { get; set; }
+
+        /// <summary>
+        /// Obtém ou define a data de cadastro da devolução do pagamento.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("dataCadastro")]
+        public DateTime? DataCadastro { get; set; }
+
+        /// <summary>
+        /// Obtém ou define o usuário cadastrado na devolução do pagamento.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("usuarioCadastro")]
+        public string UsuarioCadastro { get; set; }
+
+        /// <summary>
+        /// Obtém ou define as permissões da lista da devolução do pagamento.
+        /// </summary>
+        [DataMember]
+        [JsonProperty("permissoes")]
+        public PermissoesDto Permissoes { get; set; }
+    }
+}
---
+++ Glass/Vue/_Compartilhado/Servicos/DevolucoesPagamento.js
@@ -0,0 +1,34 @@
+﻿var Servicos = Servicos || {};
+
+/**
+ * Objeto com os serviços para a API de devoluções de pagamento.
+ */
+Servicos.DevolucoesPagamento = (function (http) {
+    const API = '/api/v1/devolucoesPagamento/';
+
+    return {
+        /**
+         * Recupera a lista de devoluções de pagamento.
+         * @param {?Object} filtro Objeto com os filtros a serem usados para a busca dos itens.
+         * @param {number} pagina O número da página de resultados a ser exibida.
+         * @param {number} numeroRegistros O número de registros que serão exibidos na página.
+         * @param {string} ordenacao A ordenação para o resultado.
+         * @returns {Promise} Uma promise com o resultado da busca.
+         */
+        obterLista: function (filtro, pagina, numeroRegistros, ordenacao) {
+            return http().get(API.substr(0, API.length - 1), {
+                params: Servicos.criarFiltroPaginado(filtro, pagina, numeroRegistros, ordenacao)
+            });
+        },
+
+        /**
+         * Recupera a lista de situações para uso no controle de devoluções de pagamento.
+         * @returns {Promise} Uma promise com o resultado da busca.
+         */
+        obterSituacoes: function () {
+            return http().get(API + 'situacoes');
+        }
+    };
+})(function () {
+    return Vue.prototype.$http;
+});
