b16c68367d2b176c09131b72c38cef80e43c08ee
Isabela Lima <isabelalima@syncsoftwares.com.br>
2019-02-01 12:20:56 -0200

    #2591-Correção identação

--- Glass/Vue/_Compartilhado/Servicos/AcertosCheques.js
+++ Glass/Vue/_Compartilhado/Servicos/AcertosCheques.js
@@ -6,21 +6,21 @@
 Servicos.AcertosCheques = (function (http) {
   const API = '/api/v1/acertosCheques/';
 
-    return {
-        /**
-         * Recupera a lista de acertos de cheques.
-         * @param {?Object} filtro Objeto com os filtros a serem usados para a busca dos itens.
-         * @param {number} pagina O número da página de resultados a ser exibida.
-         * @param {number} numeroRegistros O número de registros que serão exibidos na página.
-         * @param {string} ordenacao A ordenação para o resultado.
-         * @returns {Promise} Uma promise com o resultado da busca.
-         */
-        obterListaAcertoCheque: function (filtro, pagina, numeroRegistros, ordenacao) {
-            return http().get(API.substr(0, API.length - 1), {
-                params: Servicos.criarFiltroPaginado(filtro, pagina, numeroRegistros, ordenacao)
-            });
-        },
-    };
+  return {
+    /**
+     * Recupera a lista de acertos de cheques.
+     * @param {?Object} filtro Objeto com os filtros a serem usados para a busca dos itens.
+     * @param {number} pagina O número da página de resultados a ser exibida.
+     * @param {number} numeroRegistros O número de registros que serão exibidos na página.
+     * @param {string} ordenacao A ordenação para o resultado.
+     * @returns {Promise} Uma promise com o resultado da busca.
+     */
+    obterListaAcertoCheque: function (filtro, pagina, numeroRegistros, ordenacao) {
+      return http().get(API.substr(0, API.length - 1), {
+        params: Servicos.criarFiltroPaginado(filtro, pagina, numeroRegistros, ordenacao)
+      });
+    },
+  };
 })(function () {
-    return Vue.prototype.$http;
+  return Vue.prototype.$http;
 });
